4331ef79777df47ad9093ec77af12c0156113255
Abuzar84 <sayyedabuzar021@gmail.com>
2025-02-08 13:38:43 +0000

    pdf drop half work complete

--- pdfeditor.js
+++ pdfeditor.js
@@ -1,2 +1,23 @@
+const dropArea = document.getElementById("drop_area");
+const fileInput = document.getElementById("file_input");
 
 
+fileInput.addEventListener("change", (event) =>{
+    event.preventDefault();
+    console.log(fileInput.name);
+});
+dropArea.addEventListener("drop", (event) =>{
+    event.preventDefault();
+    dropArea.classList.remove("hover");
+    const file = event.dataTransfer.files;
+    fileInput.files = file
+    console.log(fileInput.file);
+});
+dropArea.addEventListener("dragover", (event) =>{
+    event.preventDefault();
+    dropArea.classList.add("hover");
+});
+dropArea.addEventListener("dragleave",(event) =>{
+    event.preventDefault();
+    dropArea.classList.remove("hover");
+});
