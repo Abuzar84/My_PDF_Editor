489936178c77810332ada3a5a15ecee04448846f
Abuzar84 <sayyedabuzar021@gmail.com>
2025-02-11 13:39:19 +0000

    add server

--- pdfeditor.js
+++ pdfeditor.js
@@ -1,50 +1,59 @@
 const dropArea = document.getElementById("drop_area");
 const fileInput = document.getElementById("file_input");
-const fileArea = document.getElementById("file_area");
+const fileName = document.getElementById("file_name");
+let fileStoreForserver;
+let file;
 
-
-fileInput.addEventListener("change", (event) =>{
+fileInput.addEventListener("change", (event) => {
     event.preventDefault();
-    const file = event.target.files[0];
-    if(!file) return;
-    const fileOpen = new FileReader();
-    fileOpen.onload = function(event){
-    const typedarray = new Uint8Array(fileOpen.result);
-
-    pdfjsLib.getDocument(typedarray).promise.then(pdf =>{
-        console.log("pdf loaded");
-        pdf.getPage(1).then(page => {
-            const scale = 0.5;
-            const viewport = page.getViewport({scale});
-            const canvas = fileArea;
-            const context = canvas.getContext("2d");
-
-            canvas.width = viewport.width;
-            canvas.height = viewport.height;
-
-            const rendercontext = {canvasContext: context, viewport: viewport};
-            page.render(rendercontext);
-            return page.getTextContent();
-        }).then(textContent => {
-            let text = textContent.item.map(item => item.str).join("");
-            fileArea.textContent = text;
-        });
-    }).catch(err => console.error("error",err));
-    };
-    fileOpen.readAsArrayBuffer(file);
+    file = fileInput.files[0];
+    fileStoreForserver = file;
+    if(file){
+        if(file.type === "application/pdf"){
+            fileName.textContent = file.name;
+        }
+    }
 });
-dropArea.addEventListener("drop", (event) =>{
-    event.preventDefault();
-    dropArea.classList.remove("hover");
-    const file = event.target.files;
-    fileInput.file = event.dataTransfer.files;
-    document.getElementById("file_area").innerHTML = `Your File name is ${fileInput.file[0].name}`;
-});
-dropArea.addEventListener("dragover", (event) =>{
+dropArea.addEventListener("dragover",(event) => {
     event.preventDefault();
     dropArea.classList.add("hover");
+    fileInput.style.display = "none";
 });
-dropArea.addEventListener("dragleave",(event) =>{
+dropArea.addEventListener("dragleave", (event) => {
     event.preventDefault();
     dropArea.classList.remove("hover");
+    fileInput.style.display = "block";
 });
+dropArea.addEventListener("drop", (event) => {
+    event.preventDefault();
+    dropArea.classList.remove("hover");
+    fileInput.style.display = "block";
+    file = event.dataTransfer.files;
+    fileStoreForserver = file;
+    if(file){
+        const fileNameShow = file[0].name;
+        if(file[0].type === "application/pdf"){
+            fileName.textContent = fileNameShow;
+            fileInput.files = file;
+        }
+        fileName.textContent = "Only PDF files are allowed. Please upload a valid PDF.";
+    }
+
+});
+
+async function uploadFile() {
+    if(!fileStoreForserver.files.lenth){
+        alert("Please select a file!");
+        return;
+    }
+
+    const dataStore = new FormData();
+    dataStore.append("file",fileStoreForserver[0]);
+
+    const response = await fetch("http://localhost:3000/upload", {
+        method: "post",
+        body: dataStore,
+    });
+    const result = await response.json();
+    alert(result.message);// success or error message
+}
---
+++ server.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const multer = require("multer");
+const express = require("cors");
